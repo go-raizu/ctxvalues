f91511296b8f222eb0b1de056e0ad57f921a2194
aezhar <104122316+aezhar@users.noreply.github.com>
2022-11-19 16:21:58 +0100

    add some more documentation

--- mod.go
+++ mod.go
@@ -20,15 +20,21 @@ import (
 
 type Key[T any] struct{}
 
+// WithValue returns a copy of the parent context in which the value
+// associated with key type is val.
 func (k Key[T]) WithValue(ctx context.Context, val T) context.Context {
 	return context.WithValue(ctx, (*T)(nil), val)
 }
 
+// Get returns the value and a boolean value indicating whenever
+// the value was found within the context or not.
 func (k Key[T]) Get(ctx context.Context) (v T, ok bool) {
 	v, ok = ctx.Value((*T)(nil)).(T)
 	return
 }
 
+// GetOrElse returns the value if the value was found within the
+// context or the provided default value otherwise.
 func (k Key[T]) GetOrElse(ctx context.Context, val T) T {
 	if v, ok := k.Get(ctx); ok {
 		val = v
